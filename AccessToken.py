9cf702081d89c6a24e92964b90941cf310923b28
Megateadal <zjac152@live.rhul.ac.uk>
2021-02-06 12:46:16 +0000

    Create AccessToken.py

---
+++ AccessToken.py
@@ -0,0 +1,24 @@
+import sys
+
+import requests
+
+CLIENT_ID = '5ee7a648878b46cfb5f4c973736acc99'
+CLIENT_SECRET = 'ff39b32792fa4603b830accb294bf433'
+
+AUTH_URL = 'https://accounts.spotify.com/api/token'
+
+auth_response = requests.post(AUTH_URL, {
+    'grant_type': 'client_credentials',
+    'client_id': CLIENT_ID,
+    'client_secret': CLIENT_SECRET,
+})
+
+# convert the response to JSON
+auth_response_data = auth_response.json()
+
+# save the access token
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
